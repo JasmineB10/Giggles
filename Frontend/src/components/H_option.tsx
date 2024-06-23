8ec0e42fa297b256c54ebcdce3a33329d3345532
Jasmine Bajwa <bajwajas10@gmail.com>
2024-06-23 09:23:14 +0530

    Home Page Commit

--- Frontend/src/components/H_option.tsx
+++ Frontend/src/components/H_option.tsx
@@ -1,8 +1,8 @@
 
 function H_option(props:any) {
   return (
-    <div className="px-6 py-2 bg-slate-50 text-base rounded-full font-medium outline outline-blue-300 hover:bg-blue-300 hover:outline-transparent">
-      <button>{props.option}</button>
+    <div className="flex justify-center">
+      <button className="max-w-50 max-h-10 px-6 py-2 bg-slate-50 text-base rounded-full font-medium outline outline-blue-300 hover:bg-blue-300 hover:outline-transparent">{props.option}</button>
     </div>
   )
 }
--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import NavBarBtn from "./NavBarBtn"
 
 function NavBar() {
   return (
-    <div className="grid grid-rows-12 z-10 fixed top-0 w-full">
+    <div className="grid grid-rows-12 z-10 fixed top-0 w-full h-screen">
     <div className="row-span-2 grid grid-cols-7 place-items-center py-2 w-full  z-10">
     <div className="col-span-1">
       <button className="px-6 py-3 text-lg rounded-full font-bold transition ease-in-out delay-150 bg-gradient-to-r from-blue-400 via-purple-500 to-fuchsia-400 hover:-translate-y-0.5 hover:scale-110 duration-300">
@@ -29,14 +29,14 @@ function NavBar() {
                <NavBarBtn name={"SignUp"}></NavBarBtn>
         </div>
       </div>
-      <div className="row-span-10 grid grid-cols-8 z-10">
-        <div className="row-start-2 col-span-1 col-start-2">
+      <div className="row-span-10 grid grid-cols-8 z-10 pt-8">
+        <div className="row-start-2 col-span-2 col-start-1">
         <H_option option={"Conditions A-Z"}/>
         </div>
         <div className="row-start-3 col-span-1 col-start-2">
         <H_option option={"Living Well"}/>
         </div>
-        <div className="row-start-4 col-span-1 col-start-2">
+        <div className="row-start-4 col-span-2 col-start-1">
         <H_option option={"Understand Yourself"}/>
         </div>
         <div className="row-start-5 col-span-1 col-start-2">
@@ -45,13 +45,13 @@ function NavBar() {
         <div className="row-start-2 col-span-1 col-start-7">
         <H_option option={"Help"}/>
         </div>
-        <div className="row-start-3 col-span-1 col-start-7">
+        <div className="row-start-3 col-span-2 col-start-7">
           <H_option option={"Donate"}/>
         </div>
         <div className="row-start-4 col-span-1 col-start-7">
           <H_option option={"Podcast"}/>
         </div>
-        <div className="row-start-5 col-span-1 col-start-7">
+        <div className="row-start-5 col-span-2 col-start-7">
           <H_option option={"Our Programmes"}/>
         </div>
         
